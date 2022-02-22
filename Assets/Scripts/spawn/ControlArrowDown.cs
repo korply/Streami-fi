e13fa4574b78c3380c4e8a398b38055d9ce767b9
korply <87417114+AnodazTeam@users.noreply.github.com>
2022-02-22 16:53:07 +0700

    Revert "Merge branch 'main' of https://github.com/korply/Streami-fi"
    This reverts commit 301ecfa14700e4a62e151af97e408c806771e7d7, reversing
    changes made to 324cb1550c241bc9aa15122607afb8fbafe189bc.

--- Assets/Scripts/spawn/ControlArrowDown.cs
+++ Assets/Scripts/spawn/ControlArrowDown.cs
@@ -10,6 +10,7 @@ public class ControlArrowDown : MonoBehaviour
     public SpriteRenderer perfact;
     public SpriteRenderer good;
 
+    int score;
 
     float timer = 0f;
 
@@ -22,29 +23,26 @@ public class ControlArrowDown : MonoBehaviour
     {
         timer += Time.deltaTime;
         circle.size = new Vector2(1.5f - (timer), 1.5f - (timer));
-
+        
         if (timer > 1.3)
         {
             timer = 0;
             miss.gameObject.SetActive(true);
-            CurrentScore.instance.Miss(0);
-            Song.song.Miss();
+            Score.instance.NoteMissed();
             Destroy(gameObject, 0.2f);
         }
-        if (timer > 1f && timer < 1.2 && Input.GetKeyDown("down"))
+        if (timer > 1f && timer < 1.2 && Input.GetKey("down"))
         {
             perfact.gameObject.SetActive(true);
-            CurrentScore.instance.Score(100, 1);
-            Song.song.Good();
+            Score.instance.NoteHitPerfact();
             Destroy(miss);
             Destroy(good);
             Destroy(gameObject, 0.2f);
         }
-        if (timer > 0.1f && timer < 0.99f && Input.GetKeyDown("down"))
+        if (timer > 0.1f && timer < 0.99f && Input.GetKey("down"))
         {
             good.gameObject.SetActive(true);
-            CurrentScore.instance.Score(50, 1);
-            Song.song.Bad();
+            Score.instance.NoteHitGood();
             Destroy(miss);
             Destroy(perfact);
             Destroy(gameObject, 0.2f);
